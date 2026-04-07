5679928d83b26051c769d925267fd9dffbfc1bb9
Paulomillanez1983 <86369587+Paulomillanez1983@users.noreply.github.com>
2026-04-07 09:14:48 -0300

    Update mimi-driver-sim.js

--- mimi-driver-sim.js
+++ mimi-driver-sim.js
@@ -87,7 +87,7 @@ window.DriverSim = (() => {
           source: SOURCE_ID,
           layout: {
             'icon-image': IMAGE_ID,
-            'icon-size': 0.055,
+             'icon-size': window.innerWidth <= 768 ? 0.035 : 0.055,
             'icon-allow-overlap': true,
             'icon-ignore-placement': true,
             'icon-rotate': ['get', 'bearing'],
