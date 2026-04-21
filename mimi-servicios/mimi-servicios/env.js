32138b07499e0e90c94a9e4f9ffb9da11d01d8f1
PM <86369587+Paulomillanez1983@users.noreply.github.com>
2026-04-21 16:17:55 -0300

    Create env.js

---
+++ mimi-servicios/mimi-servicios/env.js
@@ -0,0 +1,4 @@
+window.MIMI_SERVICES_ENV = {
+  SUPABASE_URL: "https://xrphpqmutvadjrucqicn.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhycGhwcW11dHZhZGpydWNxaWNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ0MDY5ODgsImV4cCI6MjA4OTk4Mjk4OH0.0nsO3GBevQzMBCvne17I9L5_Yi4VPYiWedxyntLr4uM"
+};
