b2f9f2cdd14b72590a96f95c8ed6c079036e81df
PM <86369587+Paulomillanez1983@users.noreply.github.com>
2026-04-25 19:57:34 -0300

    Update main-provider.js

--- mimi-servicios/src/main-provider.js
+++ mimi-servicios/src/main-provider.js
@@ -608,9 +608,16 @@ container.innerHTML = `
     </div>
 
     <button class="provider-login-google" id="providerGoogleLoginButton" type="button">
-      <span class="provider-google-icon">G</span>
-      <span>
-        <strong>Continuar con Google</strong>
+<span class="provider-google-icon" aria-hidden="true">
+  <svg viewBox="0 0 48 48">
+    <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"/>
+    <path fill="#4285F4" d="M46.5 24.5c0-1.57-.14-3.08-.4-4.5H24v9h12.65c-.55 2.9-2.2 5.36-4.7 7.01l7.28 5.65C43.5 37.73 46.5 31.66 46.5 24.5z"/>
+    <path fill="#FBBC05" d="M10.54 28.59A14.4 14.4 0 0 1 9.75 24c0-1.6.28-3.15.79-4.59l-7.98-6.19A23.9 23.9 0 0 0 0 24c0 3.87.93 7.52 2.56 10.78l7.98-6.19z"/>
+    <path fill="#34A853" d="M24 48c6.47 0 11.9-2.13 15.87-5.79l-7.28-5.65c-2.02 1.35-4.61 2.15-8.59 2.15-6.26 0-11.57-4.22-13.46-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"/>
+  </svg>
+</span>
+
+<strong>Continuar con Google</strong>
         <small>Cuenta de prestador</small>
       </span>
     </button>
